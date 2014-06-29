958bcfa7a72f5a803d01af31c3df9be74291e972
Ken Driscoll <driscoll.ken@gmail.com>
2014-06-28 23:21:46 -0500

    Initial commit

---
+++ Test iPhone App/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  Test iPhone App
+//
+//  Created by Ken Driscoll on 6/28/14.
+//  Copyright (c) 2014 Ken Driscoll. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+                            
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
